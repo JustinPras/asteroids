72450cf7d1b1d9726af59594ae2af2bf7ea7b777
JustinPras <justinponpras@gmail.com>
2024-12-22 23:02:26 +1100

    added clock/FPS

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
+    clock = pygame.time.Clock()
+    dt = 0
+    FPS = 60
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(FPS)/1000
 
 if __name__ == '__main__':
     main()
