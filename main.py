2e9c00747632173b43aaf897699abd0a734b22cd
JustinPras <justinponpras@gmail.com>
2024-12-25 11:43:31 +1100

    added player left/right rotation

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     FPS = 60
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
         
+        player.update(dt)
         screen.fill("black")
-        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(FPS)/1000
+        
 
 if __name__ == '__main__':
     main()
